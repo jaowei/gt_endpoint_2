4f6b04e72dc1da636b9529d30b9a89f8d70a8069
jaowei <jaowei.8@gmail.com>
2022-08-24 15:38:47 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const port = 3002;
+
+router.get('/two', async (req, res) => {
+    try {
+        message = req.body.message
+
+        if (message == 'no') {
+            next = 'http://localhost:3004';
+        } else {
+            next = 'http://localhost:3003' + '/?itemName=' + message;
+        };
+
+        res_msg = {
+            'message': 'How much is this item?',
+            'next': next
+        }
+        res.status(201).send(res_msg)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.listen(port, () => {
+    console.log('server is up on', port);
+})
