7ed266db734442a74cb89052c34403bfc2c26c1b
jaowei <jaowei.8@gmail.com>
2022-08-24 15:44:12 +0800

    ammend typo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
-router.get('/two', async (req, res) => {
+app.get('/two', async (req, res) => {
     try {
         message = req.body.message
 
